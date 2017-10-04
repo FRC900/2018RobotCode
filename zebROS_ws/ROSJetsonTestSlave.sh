3bda826656ccbd42a4ce0f0b33bcac88d1c98067
Kevin Jaget <kjaget@gmail.com>
2017-10-04 10:41:04 -0400

    More cleanup of ROS env scripts

--- zebROS_ws/ROSJetsonTestSlave.sh
+++ zebROS_ws/ROSJetsonTestSlave.sh
@@ -2,7 +2,7 @@
 
 # Setup ROS for Jetson Slave
 source /opt/ros/kinetic/setup.bash
-source /home/ubuntu/2017VisionCode/zebROS_ws/install_isolated/setup.bash
+source ~/2017Preseason/zebROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://10.9.0.12:5802
 export ROS_IP=$(hostname -I)
 export ROSLAUNCH_SSH_UNKNOWN=1
--- zebROS_ws/ROSStandard.sh
+++ zebROS_ws/ROSStandard.sh
@@ -2,7 +2,7 @@
 
 # Setup ROS for Local Development
 source /opt/ros/kinetic/setup.bash
-source ~/2017VisionCode/zebROS_ws/install_isolated/setup.bash
+source ~/2017Preseason/zebROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://localhost:5802
 export ROS_IP=$(hostname -I)
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
