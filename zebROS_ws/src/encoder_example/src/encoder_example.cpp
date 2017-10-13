11795d73da8dd56171bb5b4324236f126992f7ff
Kevin Jaget <kjaget@gmail.com>
2017-10-13 09:19:01 -0400

    Switch from CanTalonSRX interface to CANTalon

--- zebROS_ws/src/encoder_example/src/encoder_example.cpp
+++ zebROS_ws/src/encoder_example/src/encoder_example.cpp
@@ -3,7 +3,7 @@
 #include <ros/ros.h>
 
 #include "encoder_example/canTalonEncoder.h"
-#include "ctrlib/CanTalonSRX.h"
+#include "ctrlib/CANTalon.h"
 
 using namespace std;
 
@@ -22,31 +22,26 @@ int main(int argc, char** argv)
 
 	ros::Rate loop_time(50); // run at 50Hz
 
-	CanTalonSRX canTalon(11); // make id number a param instead of hard-coded
+	CANTalon canTalon(11); // make id number a param instead of hard-coded
 
 	while(ros::ok()) 
 	{
 		//set the timestamp for all headers
-		int pos;
-		int vel;
-		if ((canTalon.GetEncPosition(pos) == CTR_OKAY) &&
-			(canTalon.GetEncVel(vel)      == CTR_OKAY) )
-		{
-			stampedEncoderData.header.stamp = ros::Time::now();
-			stampedEncoderData.position = pos;
-			stampedEncoderData.velocity = vel;
-			// Need to decide exactly what to publish.  For this simple
-			// example it is just a raw encoder values. For a swerve drive
-			// wheel we might need velocity in X and Y directions.  For 
-			// a shooter we might just report velocity.  For linear actuators
-			// maybe publish a 1-d position
-			//
-			// Or do we have other topics which read the raw encoder values
-			// and convert them into real-world units of position / velocity?
-			//
-			//publish to ROS topics
-			pub.publish(stampedEncoderData); // queue up data to publish
-		}
+		stampedEncoderData.header.stamp = ros::Time::now();
+		stampedEncoderData.position = canTalon.GetEncPosition();
+		stampedEncoderData.velocity = canTalon.GetEncVel();
+		// Need to decide exactly what to publish.  For this simple
+		// example it is just a raw encoder values. For a swerve drive
+		// wheel we might need velocity in X and Y directions.  For
+		// a shooter we might just report velocity.  For linear actuators
+		// maybe publish a 1-d position
+		//
+		// Or do we have other topics which read the raw encoder values
+		// and convert them into real-world units of position / velocity?
+		//
+		//publish to ROS topics
+		pub.publish(stampedEncoderData); // queue up data to publish
+
 		ros::spinOnce();        // yield and publish data
 		loop_time.sleep();      // sleep until next update period hits
 	}
