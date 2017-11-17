89074d90875616aa6b29a8de9c356750903fff16
Anja Sheppard <sheppard18a@ncssm.edu>
2017-11-16 19:30:06 -0500

    init commit for px4

--- zebROS_ws/src/px4_to_odom/src/to_odom.py
+++ zebROS_ws/src/px4_to_odom/src/to_odom.py
@@ -5,15 +5,18 @@ import rospy
 from nav_msgs.msg import Odometry
 from px_comm.msg import OpticalFlow
 
+global prev_time
+
 pub = rospy.Publisher("px4_odom", Odometry, queue_size=1)
 
 def listener ():
 	rospy.Subscriber("/px4flow/opt_flow", OpticalFlow, callback)
 
 def callback (data):
-	prev_time = cur_time
-	cur_time = rospy.get_rostime()
+	global prev_time
+	cur_time = rospy.get_rostime().to_sec()
 	dt = cur_time - prev_time
+	prev_time = cur_time
 	odom = Odometry()
 	odom.header.stamp = data.header.stamp
 	odom.header.frame_id = "odom"
@@ -21,14 +24,15 @@ def callback (data):
 	odom.pose.pose.orientation.w = 1
 	odom.twist.twist.linear.x = data.velocity_x
 	odom.twist.twist.linear.y = data.velocity_y
-	odom.pose.pose.position.x = data.velocity_x / dt
-	odom.pose.pose.position.x = data.velocity_y / dt
+	odom.pose.pose.position.x += data.velocity_x / dt
+	odom.pose.pose.position.y += data.velocity_y / dt
 	pub.publish(odom)
 
 
 if __name__ == "__main__":
+	global prev_time
 	rospy.init_node("to_odom")
-	prev_time = rospy.get_rostime()
+	prev_time = 0
 	listener()
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
