c65d6c1dba168158add9424257ba0ba3869fe4fc
Kevin Jaget <kjaget@gmail.com>
2017-11-27 11:35:29 -0500

    Clean up warning

--- zebROS_ws/src/talon_state_controller/src/talon_state_controller.cpp
+++ zebROS_ws/src/talon_state_controller/src/talon_state_controller.cpp
@@ -124,7 +124,7 @@ namespace talon_state_controller
       return;
     }
 
-    for(std::size_t i = 0; i < list.size(); ++i)
+    for(int i = 0; i < list.size(); ++i)
     {
       if (list[i].getType() != XmlRpc::XmlRpcValue::TypeStruct)
       {
