a6590fc6692f04e578fe3746d4fd10930555058f
Team900 Jetson TX2 <progammers@team900.org>
2017-11-27 19:37:35 -0500

    More work adding PIDFIzone & PIDF config

--- zebROS_ws/src/talon_interface/include/talon_interface/talon_command_interface.h
+++ zebROS_ws/src/talon_interface/include/talon_interface/talon_command_interface.h
@@ -30,8 +30,7 @@ namespace hardware_interface
 				mode_(TalonMode_Uninitialized),
 				mode_changed_(false),
 				pidf_slot_(0),
-				pidf_slot_changed_(false),
-				pidf_changed_(false)
+				pidf_slot_changed_(false)
 			{
 				for (int slot = 0; slot < 2; slot++)
 				{
@@ -40,6 +39,7 @@ namespace hardware_interface
 					d_[slot] =  0.0;
 					f_[slot] =  0.0;
 					i_zone_[slot] = 0.0;
+					pidf_changed_[slot] = true;
 				}
 			}
 			// These get the requested setpoint, not the
@@ -47,7 +47,40 @@ namespace hardware_interface
 			// Need to think about which makes the most
 			// sense to query...
 			double get(void) const {return command_;}
+
 			TalonMode getMode(void) const {return mode_;}
+
+			void setP(double oldP){
+				pidf_changed_[pidf_slot_] = true;
+				p_[pidf_slot_] = oldP;}
+			double getP(void) const {return p_[pidf_slot_]	;}
+
+			void setI(double ii){
+				pidf_changed_[pidf_slot_] = true;
+				i_[pidf_slot_] = ii;}
+			double getI(void) const {return i_[pidf_slot_];}
+			
+			void setPID(double oldP, double oldI, double oldD){
+				pidf_changed_[pidf_slot_] = true;
+				p_[pidf_slot_] = oldP;i_[pidf_slot_] =oldI;d_[pidf_slot_]=oldD;}
+			virtual void setPID(double oldP, double oldI, double oldD, double oldF){
+				pidf_changed_[pidf_slot_] = true;
+				p_[pidf_slot_]=oldP;i_[pidf_slot_]=oldI;d_[pidf_slot_]=oldD;f_[pidf_slot_]=oldF;}
+
+			void setD(double dd){
+				pidf_changed_[pidf_slot_] = true;
+				d_[pidf_slot_] = dd;}
+			double getD(void) const {return d_[pidf_slot_];}
+
+			void setF(double ff){
+				pidf_changed_[pidf_slot_] = true;
+				f_[pidf_slot_] = ff;}
+			double getF(void){return f_[pidf_slot_];}
+
+			void setIZ(unsigned oldIZ){
+				pidf_changed_[pidf_slot_] = true;
+				i_zone_[pidf_slot_] = oldIZ;}
+			unsigned getIZ(void) const {return i_zone_[pidf_slot_];}
 
 			void set(double command) {command_ = command;}
 			void setMode(const TalonMode mode)
@@ -62,7 +95,27 @@ namespace hardware_interface
 				this->set(0); // ??? Clear out setpoint for old mode
 			}
 
-			// Check to see if mode changed since last call
+			bool slotChanged(int &newpidfSlot)
+			{
+				if (!pidf_slot_changed_)
+					return false;
+				newpidfSlot = pidf_slot_;
+				pidf_slot_changed_ = false;
+				return true;
+			}
+			bool pidfChanged(double &p, double &i, double &d, double &f, unsigned &iz){
+				if (!pidf_changed_[pidf_slot_])
+					return false;
+				p = p_[pidf_slot_];
+				i = i_[pidf_slot_];
+				d = d_[pidf_slot_];
+				f = f_[pidf_slot_];
+				iz = i_zone_[pidf_slot_];
+				pidf_slot_changed_ = false;
+				return true;
+			}
+
+			// Check  to see if mode changed since last call
 			// If so, return true and set mode to new desired
 			// talon mode
 			// If mode hasn't changed, return false
@@ -79,22 +132,24 @@ namespace hardware_interface
 				return true;
 			}
 
+			void setPidfSlot(int npidf_slot){pidf_slot_ = npidf_slot;pidf_slot_changed_ = true;}
+			int getPidfSlot(void)const{return pidf_slot_;}
+
 		private:
 			double    command_; // motor setpoint - % vbus, velocity, position, etc
-
 			TalonMode mode_;         // talon mode - % vbus, close loop, motion profile, etc
 			bool      mode_changed_; // set if mode needs to be updated on the talon hw
-
+			double    ramprate;
 			int       pidf_slot_; // index 0 or 1 of the active PIDF slot
 			bool      pidf_slot_changed_; // set to true to trigger a write to PIDF select on Talon
 
 			// 2 entries in the Talon HW for each of these settings
 			double    p_[2];
 			double    i_[2];
-			double    i_zone_[2];
+			unsigned    i_zone_[2];
 			double    d_[2];
 			double    f_[2];
-			bool      pidf_changed_;
+			bool      pidf_changed_[2];
 	};
 
 	// Handle - used by each controller to get, by name of the
