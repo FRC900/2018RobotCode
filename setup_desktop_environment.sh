26e5f00cc9805ab473250cd10bd3e80cd92468a0
Unknown <dapearce@students.wcpss.net>
2017-12-23 19:28:30 -0500

    setup_desktop_environment.sh changes
    combined all ROS install steps into one string

--- setup_desktop_environment.sh
+++ setup_desktop_environment.sh
@@ -12,10 +12,10 @@ sudo apt-get install libflann-dev libpcl-dev
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 
-echo "Do you wish to install the ROS repositories? Select NO if they are already installed"
+echo "Do you wish to install ROS? Select No if already installed"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116; break;;
+        Yes ) sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 && sudo apt update && sudo apt install -y ros-kinetic-desktop-full && source /opt/ros/kinetic/setup.bash && sudo rosdep init && rosdep update; break;;
         No ) break;;
     esac
 done
@@ -67,6 +67,3 @@ select yn in "Yes" "No"; do
         No ) break;;
     esac
 done
-source /opt/ros/kinetic/setup.bash
-sudo rosdep init
-rosdep update
