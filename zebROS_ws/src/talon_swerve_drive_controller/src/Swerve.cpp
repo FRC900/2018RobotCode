6c6fe94e8d9fe3400122684a0c47d4c4e6cf4870
Ryan Greenblatt <greenblattryan@gmail.com>
2018-01-13 21:07:27 -0500

    Maybe fixed nearest angle?

--- zebROS_ws/src/talon_swerve_drive_controller/src/Swerve.cpp
+++ zebROS_ws/src/talon_swerve_drive_controller/src/Swerve.cpp
@@ -64,7 +64,7 @@ array<Vector2d, WHEELCOUNT> swerve::motorOutputs(Vector2d velocityVector, double
 			nearestangle = leastDistantAngleWithinHalfPi(encoderPosition_[i], speedsAndAngles[i][1], reverse);
 			reverses[i] = reverse;
 			speedsAndAngles[i][0] *= ((drive_.maxSpeed / (drive_.wheelRadius)) / ratio_.encodertoRotations) * units_.rotationSetV * (reverse ? -1 : 1);
-			speedsAndAngles[i][1] *= units_.steeringSet;
+			speedsAndAngles[i][1] = nearestangle * units_.steeringSet;
 			speedsAndAngles[i][1] -= offsets_[i];
 		}
 	}
@@ -75,11 +75,11 @@ array<Vector2d, WHEELCOUNT> swerve::motorOutputs(Vector2d velocityVector, double
 			speedsAndAngles[i][1] = swerveMath_.parkingAngle_[i]; // TODO : find a way not to access member of swervemath here
 			speedsAndAngles[i][0] = 0;
 
-			double nearestangle;
+			double nearestanglep;
 			bool reverse;
 			getWheelAngle(i, encoderPosition_[i]);
-			nearestangle = leastDistantAngleWithinHalfPi(encoderPosition_[i], speedsAndAngles[i][1], reverse);
-			speedsAndAngles[i][1] *= units_.steeringSet;
+			nearestanglep = leastDistantAngleWithinHalfPi(encoderPosition_[i], speedsAndAngles[i][1], reverse);
+			speedsAndAngles[i][1] = nearestanglep units_.steeringSet;
 			speedsAndAngles[i][1] -= offsets_[i];
 		}
 
