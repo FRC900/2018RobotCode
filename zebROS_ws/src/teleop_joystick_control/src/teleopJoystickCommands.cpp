f67ebf58939705087d1195ac63c924d8f963069b
FalconWarriorr <niall.p.mullane@gmail.com>
2018-01-19 20:50:07 -0500

    right joystick y axis publishing

--- zebROS_ws/src/teleop_joystick_control/src/teleopJoystickCommands.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/teleopJoystickCommands.cpp
@@ -2,6 +2,7 @@
 #include "ros/console.h"
 #include "ros_control_boilerplate/JoystickState.h"
 #include "teleop_joystick_control/RobotState.h"
+#include "talon_controllers/CloseLoopControllerMsg.h"
 #include "geometry_msgs/Twist.h"
 #include "ros/time.h"
 #include <string>
@@ -11,6 +12,7 @@ bool ifCube;
 double elevatorHeight;
 static double timeSecs, lastTimeSecs;
 static ros::Publisher JoystickRobotVel;
+static ros::Publisher JoystickArmVel;
 void evaluateCommands(const ros_control_boilerplate::JoystickState::ConstPtr &msg) {
     bool ifcube = false;
     char currentToggle = ' ';
@@ -103,16 +105,23 @@ void evaluateCommands(const ros_control_boilerplate::JoystickState::ConstPtr &ms
     ros::Rate loop_rate(10);
     while(ros::ok()) {
         geometry_msgs::Twist vel;
+        talon_controllers::CloseLoopControllerMsg arm;
         double leftStickX = msg->leftStickX;//TODO publish twist message for drivetrain and elevator/pivot
         double leftStickY = msg->leftStickY;
+
+        double rightStickX = msg->rightStickX;//TODO publish twist message for drivetrain and elevator/pivot
+        double rightStickY = msg->rightStickY;
         vel.linear.x = leftStickX;
         vel.linear.y = leftStickY;
         vel.linear.z = 0;
 
         vel.angular.x = 0;
         vel.angular.y = 0;
+
+        arm.command = rightStickY;
         vel.angular.z = msg->leftTrigger-msg->rightTrigger;
         JoystickRobotVel.publish(vel);
+        JoystickArmVel.publish(arm);
         
 
         ros::spinOnce();
@@ -161,6 +170,9 @@ int main(int argc, char **argv) {
     ros::NodeHandle r;
     JoystickRobotVel = r.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
+    ros::init(argc, argv, "arm_controller");
+    ros::NodeHandle a;
+    JoystickArmVel = a.advertise<talon_controllers::CloseLoopControllerMsg>("frcrobot/talon_linear_controller/command", 1);
     ros::spin();
 
     return 0;
