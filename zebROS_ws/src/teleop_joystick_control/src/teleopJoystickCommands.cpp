86ae2055b0ca39df431c31bb4ff78feae53651ac
FalconWarriorr <niall.p.mullane@gmail.com>
2018-01-26 09:34:08 -0500

    Edited Header File

--- zebROS_ws/src/teleop_joystick_control/src/teleopJoystickCommands.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/teleopJoystickCommands.cpp
@@ -1,17 +1,5 @@
-#include "ros/ros.h"
-#include "ros/console.h"
-#include "ros_control_boilerplate/JoystickState.h"
-#include "teleop_joystick_control/RobotState.h"
-#include "talon_controllers/CloseLoopControllerMsg.h"
-#include "ros_control_boilerplate/MatchSpecificData.h"
-#include "std_msgs/Float64.h"
-#include "geometry_msgs/Twist.h"
 #include "teleop_joystick_control/teleopJoystickCommands.h"
-#include "ros/time.h"
-#include "message_filters/subscriber.h"
-#include "message_filters/synchronizer.h"
-#include "message_filters/sync_policies/approximate_time.h"
-#include <string>
+
 
 //using namespace message_filters;
 static double timeSecs, lastTimeSecs;
