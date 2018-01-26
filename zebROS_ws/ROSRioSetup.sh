c35bd478065bcda50f0735c29fc7b816256f0d6a
Kevin Jaget <kjaget@gmail.com>
2018-01-25 21:37:24 -0500

    Move RTC config stuff into roborio_install_ros.sh

--- zebROS_ws/ROSRioSetup.sh
+++ zebROS_ws/ROSRioSetup.sh
@@ -7,21 +7,4 @@ export ROS_MASTER_URI=http://10.9.0.2:11311
 export ROS_IP=$(hostname -I)
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-
-echo \#!/bin/bash > setupClock
-echo NAME="Setup Clock" >> setupClock
-echo USER=admin >> setupClock
-echo depmod echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device >> setupClock
-echo i2cdetect -y 2 >> setupClock
-echo hwclock.util-linux --hctosys >> setupClock
-
-chmod +x setupClock
-cp setupClock /etc/init.d
-/usr/sbin/update-rc.d -f setupClock defaults
-
-
-
-
-
-
 exec "$@"
