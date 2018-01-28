26a047655f2efc21fab18b23b06be7479d4143db
Ryan Greenblatt <greenblattryan@gmail.com>
2018-01-28 14:12:58 -0500

    Changed from node to controller, began development

--- zebROS_ws/src/elevator_controller/include/elevator_node/linear_control.h
+++ zebROS_ws/src/elevator_controller/include/elevator_node/linear_control.h
@@ -1,8 +1,90 @@
 //make this file so include is uploaded to github for structure
 //
 #include "ros/ros.h"
+#include <controller_interface/controller.h>
+#include <talon_controllers/talon_controller_interface.h>
+#include <pluginlib/class_list_macros.h>
 
-void elevatorHeightControl();
-void clampStateControl();
-void pivotAngleControl();
-void evaluateCubeState();
+#include <sensor_msgs/JointState.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/tfMessage.h>
+
+#include <realtime_tools/realtime_buffer.h>
+#include <realtime_tools/realtime_publisher.h>
+
+//#include <talon_swerve_drive_controller/odometry.h>
+#include <talon_swerve_drive_controller/speed_limiter.h>
+
+#include <talon_swerve_drive_controller/Swerve.h>
+#include <array>
+#include <memory>
+#include <Eigen/Dense>
+
+
+namespace elevator_controller
+{
+
+class ElevatorController
+        : public controller_interface::Controller<hardware_interface::TalonCommandInterface>
+{
+        public:
+                ElevatorController();
+		bool init(hardware_interface::TalonCommandInterface *hw,
+			  ros::NodeHandle &root_nh,
+			  ros::NodeHandle &controller_nh);
+
+		void update(const ros::Time &time, const ros::Duration &period);
+		
+
+		void starting(const ros::Time &time);
+
+                /**
+                 * \brief Stops controller
+                 * \param time Current time
+                 */
+                void stopping(const ros::Time & /*time*/);
+
+	private:
+		bool if_cube_;
+		bool clamp_cmd_;
+		double intake_power_;
+		
+		struct IntakePistonCommand //This struct is highly subject to change
+		{			
+			bool pivot_up;
+			double left_pressure;
+			double right_pressure;
+	
+		}
+		ros::Publisher RobotStatePub;
+		elevator_teleop_control::RobotState RobotStateMsg;
+
+		talon_controllers::TalonLinearMotionMagicCloseLoopControllerInterface lift_joint_;
+                talon_controllers::TalonAngleMotionMagicCloseLoopControllerInterface pivot_joint_;
+		talon_controllers::TalonPercentOutputController intake_joint_;
+
+		struct Commands
+                {
+                        Eigen::Vector2d lin;
+                        bool up_or_down;
+                        ros::Time stamp;
+
+                        Commands() : lin({0.0, 0.0}), up_or_down(true), stamp(0.0) {}
+                };
+		realtime_tools::RealtimeBuffer<Commands> command_;
+                Commands command_struct_;
+		ros::Subscriber sub_command_;
+		double arm_length_;
+		void cmdPosCallback(const elevator_controller::ElevatorControl::ConstPtr &command);
+		void clampCallback(const std_msgs::Bool::ConstPtr &command); 
+		//TODO: add odometry		
+		//void compOdometry(const ros::Time& time, const double inv_delta_t);
+		void intakePowerCallback(const std_msgs::Float64::ConstPtr &power); 
+		//Add stuff for intake pneumatics, probably needs to be a custom msg
+		void evaluateCubeState();
+		//Something for getting the soft limit bounding boxes
+		
+
+
+}//Class
+}//Namespace
--- zebROS_ws/src/elevator_controller/src/elevator_control.cpp
+++ zebROS_ws/src/elevator_controller/src/elevator_control.cpp
@@ -1,36 +1,74 @@
 //this file exists so src folder is uploaded for structure
 #include "elevator_node/linear_control.h"
 
-bool ifCube;
-double elevatorHeight;
-double pivotAngle;
-bool clampState;
-ros::Publisher RobotStatePub;
-elevator_teleop_control::RobotState RobotStateMsg;
+namespace elevator_controller
+{
+ElevatorController::ElevatorController():
+	if_cube(false),
+	clamp_cmd_(false),
+	intake_power_(0.0),
+	arm_length_(0.0)
+{
+}	
 
-using namespace std;
-
-void elevatorHeightControl( ){
-     //TODO : Set the elevator height to which specified by the joystick control node    
-}
-
-void clampStateControl(){
-     //TODO : Control the pneumatics to open/close clamp
-}
-
-void pivotAngleControl(){
-     //TODO : set the pivot angle to which specified by the joystick control node
-}
- 
-void evaluateCubeState(){
+void ElevatorController::evaluateCubeState(){
      //TODO : get state of linebreak and publish cube holding state
 }
 
-void main(int argc, int *argv){
- /* ros::init(argc, argv, "elevator_control");
+ 
+bool ElevatorController::init(hardware_interface::TalonCommandInterface *hw,
+                			             ros::NodeHandle &root_nh,
+             	                        	     ros::NodeHandle &controller_nh)
+{
+	const std::string complete_ns = controller_nh.getNamespace();
+        std::size_t id = complete_ns.find_last_of("/");
+        name_ = complete_ns.substr(id + 1);
+	
+	
+	string lift_name;
+	string pivot_name;
+	string intake_name;
+	controller_nh.getParam("lift", lift_name);
+	controller_nh.getParam("intake", intake_name);
+	controller_nh.getParam("pivot", pivot_name);
+	
+	
+	controller_nh.getParam("arm_length", arm_length_);
+	
+	ros::NodeHandle nh(controller_nh, lift_name);
+	lift_offset_ = 0;
+	if (!nh.getParam("offset", lift_offset_))
+                        ROS_ERROR_STREAM("Can not read offset for " << lift_name);
+		
+	ros::NodeHandle nh(controller_nh, pivot_name);
+	pivot_offset_ = 0;
+	if (!nh.getParam("offset", pivot_offset_))
+                        ROS_ERROR_STREAM("Can not read offset for " << pivot_name);
+
+	ROS_INFO_STREAM_NAMED(name_,
+                             "Adding pivot with joint name: "   << pivot_name
+                             << " and lift with joint name: "   << lift_name
+			     << " and intake with joint name: " << intake_name);
+	ros::NodeHandle l_nh(controller_nh, pivot_name);
+	pivot_joint_.initWithNode(hw, nullptr, l_nh);		
+	ros::NodeHandle l_nh(controller_nh, lift_name);
+	lift_joint_.initWithNode(hw, nullptr, l_nh);		
+	ros::NodeHandle l_nh(controller_nh, intake_name);
+	intake_joint_.initWithNode(hw, nullptr, l_nh);		
+	
+	
+	
+	//TODO: something here to get bounding boxes etc.
+
+	
+
+/* ros::init(argc, argv, "elevator_control");
   ros::NodeHandle n;
   RobotStatePub = n.advertise<elevator_teleop_control::RobotState>("RobotState", 1);
   ros::Rate loop_rate(10);
   RobotStatePub.publish(RobotStateMsg);
   ros::spin();*/
+}
+
+
 }
