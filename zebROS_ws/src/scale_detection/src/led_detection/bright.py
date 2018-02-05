95db911e07fccbe3f350bd48574d5cff499832c8
Team900 Jetson TX2 <progammers@team900.org>
2018-02-05 17:52:15 -0500

    cleaned up branch

--- zebROS_ws/src/scale_detection/src/led_detection/bright.py
+++ zebROS_ws/src/scale_detection/src/led_detection/bright.py
@@ -1,8 +1,7 @@
 # USAGE
-# python bright.py --image images/retina.png --radius 41 --thresh 50
-# python bright.py --image images/retina-noise.png --radius 41 --thresh 50
-# python bright.py --image images/moon.png --radius 61 --thresh 50
-# python bright.py --image ../LedDetector/data/output/600.jpg --radius 41 --thresh 60
+# python bright.py --image images/both.jpg --radius 61 --thresh 41
+# python bright.py --image images/red.jpg --radius 61 --thresh 41
+# python bright.py --image images/blue.jpg --radius 61 --thresh 41
 
 # import the necessary packages
 from imutils import contours
@@ -11,10 +10,6 @@ import numpy as np
 import argparse
 import imutils
 import cv2
-
-
-# -- Comment in during runtime --
-#import MatchSpecificData.h
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -31,40 +26,11 @@ if image != None:
     detect = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Check what color alliance we are
-alliance_color = 1
-
-
-#If our alliance color is blue, the robot will only detect blue.
-#If our alliance color is red, the robot will detect both leds, but
-
-#Filter out anything that's not blue
-if alliance_color == 0:
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower_red = np.array([110,50,50])
-        upper_red = np.array([130,255,255])
- 
-	# Here we are defining range of bluecolor in HSV
-	# This creates a mask of blue coloured 
-	# objects found in the frame.
-	mask = cv2.inRange(hsv, lower_red, upper_red)
-	 
-	# The bitwise and of the frame and mask is done so 
-	# that only the blue coloured objects are highlighted 
-	# and stored in res
-	res = cv2.bitwise_and(image,image, mask = mask)
-	gray_ = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-	cv2.imshow("res", gray_)
-else:
-	gray_ = gray.copy()
-
 # apply thresholding to single out the brightest spots of the image
-gray_thresh = cv2.threshold(gray_, args["thresh"], 255, cv2.THRESH_BINARY)[1]
-gray_thresh = cv2.erode(gray_thresh, None, iterations=5)
-gray_thresh = cv2.dilate(gray_thresh, None, iterations=5)
-cv2.imshow("gray_thresh", gray_thresh)
-
-
+thresh = cv2.threshold(gray, args["thresh"], 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.erode(thresh, None, iterations=5)
+thresh = cv2.dilate(thresh, None, iterations=5)
+cv2.imshow("thresh", thresh)
 
 # apply a Gaussian blur to the image then find the brightest
 # region
@@ -81,7 +47,7 @@ cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
 
 # find the contours in the mask, then sort them from left to
 # right
-cnts = cv2.findContours(gray_thresh.copy(), cv2.RETR_EXTERNAL,
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 cnts = contours.sort_contours(cnts)[0]
@@ -95,8 +61,6 @@ for (i, c) in enumerate(cnts):
 	if int(circle_radius) < 18:
         	print(int(cX), int(cY))
 		cv2.circle(detect, (int(cX), int(cY)), int(circle_radius),(0, 255, 0), 2)
-
-	
 
 # show the output image
 cv2.imshow("Final", detect)
