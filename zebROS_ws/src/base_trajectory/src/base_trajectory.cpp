d670c2647ea3978e211e1360b4cb59059f02f83f
Kevin Jaget <kjaget@gmail.com>
2018-02-12 09:18:17 -0500

    Add comments to base_trajectory code

--- zebROS_ws/src/base_trajectory/src/base_trajectory.cpp
+++ zebROS_ws/src/base_trajectory/src/base_trajectory.cpp
@@ -26,18 +26,25 @@ void callback(const JointTrajectoryConstPtr& msg)
 		return;
 	}
 
+	// Hard code 3 dimensions for paths to
+	// follow - x&y translation and z rotation
 	std::vector<std::string> joint_names = {"x_linear_joint", "y_linear_joint", "z_rotation_joint"};
 	const size_t n_joints = joint_names.size();
 	std::vector<bool> angle_wraparound;
 
+	// Assume the path starts at time 0
 	ros::Time next_update_time = ros::Time(0) + period;
 	ros::Time next_update_uptime = next_update_time;
 
+	// Set this to false to prevent the code
+	// from thinking we're driving rotation joints
+	// rather than running linear mition
 	for (size_t i = 0; i < joint_names.size(); i++)
 		angle_wraparound.push_back(false);
 
+	// Allocate memory to hold an
+	// initial trajectory
 	Trajectory hold_trajectory;
-	// Initialize trajectory with all joints
 	typename Segment::State current_joint_state = typename Segment::State(1);
 	for (unsigned int i = 0; i < n_joints; ++i)
 	{
@@ -50,6 +57,13 @@ void callback(const JointTrajectoryConstPtr& msg)
 		hold_trajectory.push_back(joint_segment);
 	}
 
+	// This generates a starting trajectory
+	// with the robot sitting still at location 0,0,0.
+	// It is needed as an initial condition for the
+	// robot to connect it to the first waypoint
+	// TODO : make the starting position and 
+	// velocity a variable passed in to the 
+	// path generation request.
 	{
 		typename Segment::State hold_start_state = typename Segment::State(1);
 		typename Segment::State hold_end_state = typename Segment::State(1);
@@ -67,7 +81,7 @@ void callback(const JointTrajectoryConstPtr& msg)
 			hold_start_state.acceleration[0] = 0.0;
 
 			hold_end_state.position[0]       = 0.0;
-			hold_end_state.velocity[0]       = 0.0;
+			hold_end_state.velocity[0]       = -0.0;
 			hold_end_state.acceleration[0]   = 0.0;
 
 			hold_trajectory[i].front().init(start_time, hold_start_state, end_time_2x, hold_end_state);
@@ -79,11 +93,12 @@ void callback(const JointTrajectoryConstPtr& msg)
 			hold_trajectory[i].front().init(start_time, hold_start_state, end_time, hold_end_state);
 		}
 	}
-	ros::Time init = ros::Time::now();
 
+	ros::Time init = ros::Time::now();
 	std::cout << "Init time = " << (init - start).toSec() << std::endl;
 
-	Trajectory trajectory;
+	// Set basic options for the trajectory
+	// generation controller
 	joint_trajectory_controller::InitJointTrajectoryOptions<Trajectory> options;
 	options.other_time_base           = &next_update_uptime;
 	options.current_trajectory        = &hold_trajectory;
@@ -93,6 +108,12 @@ void callback(const JointTrajectoryConstPtr& msg)
 	options.default_tolerances        = NULL;
 	options.allow_partial_joints_goal = true;
 
+	// Actually generate the new trajectory
+	// This will create spline coefficents for
+	// each of the x,y,z paths
+	// TODO : take input from a service rather
+	// than a topic
+	Trajectory trajectory;
 	try
 	{
 		trajectory = joint_trajectory_controller::initJointTrajectory<Trajectory>(*msg, next_update_time, options);
@@ -116,19 +137,35 @@ void callback(const JointTrajectoryConstPtr& msg)
 	ros::Time gen = ros::Time::now();
 	std::cout << "Gen time = " << (gen - init).toSec() << std::endl;
 
+	// Generate and publish output message
+	// TODO : make this the results of the 
+	// service
 	trajectory_msgs::JointTrajectory out_msg;
 	out_msg.header.stamp = ros::Time::now();
 
-	ros::Duration start_time(0);
-	ros::Duration end_time = msg->points.back().time_from_start;
+	// Add names of the joints to the message
 	for (auto it = joint_names.cbegin(); it != joint_names.cend(); ++it)
 		out_msg.joint_names.push_back(*it);
 
+	// Figure out how many points are going to be 
+	// calculated along the  path. 
+	// TODO :should be a simple divide, worry about
+	// boundary conditions
 	size_t point_count = 0;
+	const ros::Duration end_time = msg->points.back().time_from_start;
 	for (ros::Duration now(0); now <= end_time; now += period)
 		point_count += 1;
 
+	// Resize the message to hold that many points
 	out_msg.points.resize(point_count);
+
+	// Loop through and generate 1 point per timestep
+	// each point has a pos/vel/acc data for each of the
+	// three x,y,z paths
+	// TODO : this takes the majority of the runtime
+	//        it is also a prime target for multi-threading
+	//        since each point is generated independently of
+	//        the last.  
 	point_count = 0;
 	for (ros::Duration now(0); now <= end_time; now += period, point_count += 1)
 	{
