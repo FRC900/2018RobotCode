1309e4d6526d241ed3c3ee9c397057cf82a11afe
Team900 Jetson TX2 <progammers@team900.org>
2018-02-14 19:16:02 -0500

    made changes to cube detection

--- zebROS_ws/src/cube_detection/src/cube_detect.cpp
+++ zebROS_ws/src/cube_detection/src/cube_detect.cpp
@@ -9,11 +9,7 @@
 #include <message_filters/sync_policies/approximate_time.h>
 
 #include <cv_bridge/cv_bridge.h>
-//C
-#include <stdio.h>
-//C++
 
-#include <sstream>
 int erosion_size = 1;
 const int sliderMax = 255;
 
@@ -26,7 +22,6 @@ static bool down_sample = false;
 
 void callback(const ImageConstPtr &frameMsg, const ImageConstPtr &depthMsg)
 {
-
 	cv_bridge::CvImageConstPtr cvFrame = cv_bridge::toCvShare(frameMsg, sensor_msgs::image_encodings::BGR8);
 	cv_bridge::CvImageConstPtr cvDepth = cv_bridge::toCvShare(depthMsg, sensor_msgs::image_encodings::TYPE_32FC1);
 	
@@ -53,71 +48,45 @@ void callback(const ImageConstPtr &frameMsg, const ImageConstPtr &depthMsg)
 		depthPtr = &depth;
 	}
 
-	//convert to grayscale and threshold
-	cvtColor(*framePtr, gray, cv::COLOR_BGR2GRAY);
- 	threshold(gray, thresh, 50, 255, 0);
+	Mat hsv;
+	Mat threshold;
+	Mat contour;
 
-	VideoCapture cap(1);
-	while(1)
+	inRange(hsv, Scalar(20,89,70),Scalar(35,255,241),threshold);
+	/*Mat threshold_channels[3];
+	split(threshold,threshold_channels);
+	mask = threshold_channels[0];*/
+
+	erode(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
+	dilate(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
+	dilate(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
+	erode(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
+
+	vector<vector<Point> > contours;
+	vector<Vec4i> rank;
+
+	findContours(threshold,contours, rank, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+
+	vector<vector<Point> > contours_poly( contours.size() );
+	vector<Rect> boundRect(contours.size());
+
+	for(int i = 0; i < contours.size(); i++)
 	{
-		Mat frame;
-		Mat hsv;
-		Mat threshold;
-		Mat edges;
-		Mat contour;
-		Mat mask;
-		Mat grey;
-		Mat out;
-		cap >> frame;
-		cvtColor(frame,hsv,COLOR_BGR2HSV);
-		cvtColor(frame,grey,COLOR_BGR2GRAY);
-		inRange(hsv,Scalar(20,89,70),Scalar(35,255,241),threshold);
-		Mat threshold_channels[3];
-		split(threshold,threshold_channels);
-		mask = threshold_channels[0];
-		//threshold(hsv,threshold,25,32,THRESH_BINARY);
-		erode(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
-		dilate(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
-		dilate(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
-		erode(threshold,threshold,getStructuringElement(MORPH_ELLIPSE,Size(4,4)));
-		//cvtColor(threshold,threshold,COLOR_HSV2BGR);	
-		//Canny(grey,edges,0,30,3);
-		//cvtColor(threshold,threshold,COLOR_BGR2GRAY);	
-		
-		
-		//edges.copyTo(out,mask); //There needs to be a line which converts the hsv image into greyscale, then the contours need to be found
-		vector<vector<Point> > contours;
-		vector<Vec4i> rank;
-		//RNG rng(12345);
-		findContours(threshold,contours, rank, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
-		
-		vector<vector<Point> > contours_poly( contours.size() );
-		vector<Rect> boundRect( contours.size() );
-		
-		for(int i = 0; i < contours.size(); i++)
-		{
-			approxPolyDP(Mat(contours[i]),contours_poly[i], 3, true);
-			boundRect[i] = boundingRect(Mat(contours_poly[i]));
-		}
-		Mat drawing = Mat::zeros(threshold.size(),CV_8UC3);
-		for(int i = 0; i<contours.size(); i++)
-		{
-			Scalar color = Scalar(0,255,0);
-			drawContours(drawing, contours,i,color,2,8,rank,0,Point());
-			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-		}
-		
-		imshow("threshold",threshold);
-		imshow("hsv",hsv);
-		//imshow("edges",edges);
-		//imshow("mask",mask);
-		//imshow("edges",edges);
-		//imshow("out",out);
-		imshow("drawing",drawing);
-		//imshow("contours",contour);
-		if((waitKey(30)) == 27 ) break;
+		approxPolyDP(Mat(contours[i]),contours_poly[i], 3, true);
+		boundRect[i] = boundingRect(Mat(contours_poly[i]));
 	}
-	//return 0;
+	Mat drawing = Mat::zeros(threshold.size(),CV_8UC3);
+	for(int i = 0; i<contours.size(); i++)
+	{
+		Scalar color = Scalar(0,255,0);
+		drawContours(drawing, contours,i,color,2,8,rank,0,Point());
+		rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+	}
+
+	imshow("threshold",threshold);
+	imshow("hsv",hsv);
+	imshow("drawing",drawing);
+	waitKey(5);
 }
 
 int main(int argc, char **argv)
@@ -138,4 +107,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
