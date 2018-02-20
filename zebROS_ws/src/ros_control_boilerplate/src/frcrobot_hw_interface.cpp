61a64b7a1f793e2773c14747a4db620dfb297b24
Kevin Jaget <kjaget@gmail.com>
2018-02-20 18:16:56 -0500

    Clear talon sticky faults in frcrobot hw interface init()

--- zebROS_ws/src/ros_control_boilerplate/src/frcrobot_hw_interface.cpp
+++ zebROS_ws/src/ros_control_boilerplate/src/frcrobot_hw_interface.cpp
@@ -244,6 +244,7 @@ void FRCRobotHWInterface::init(void)
 		can_talons_.push_back(std::make_shared<ctre::phoenix::motorcontrol::can::TalonSRX>(can_talon_srx_can_ids_[i]));
 		can_talons_[i]->Set(ctre::phoenix::motorcontrol::ControlMode::Disabled, 50); // Make sure motor is stopped, use a long timeout just in case
 		safeTalonCall(can_talons_[i]->GetLastError(), "Initial Set(Disabled, 0)");
+		safeTalonCall(can_talons_[i]->ClearStickyFaults(timeoutMs), "ClearStickyFaults()");
 		// TODO : if the talon doesn't initialize - maybe known
 		// by -1 from firmware version read - somehow tag
 		// the entry in can_talons_[] as uninitialized.
