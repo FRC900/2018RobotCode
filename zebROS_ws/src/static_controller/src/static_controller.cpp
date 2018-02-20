4cc883ec1b4a1891cf0b7b1e4bd551d6bf6d8074
Kevin Jaget <kjaget@gmail.com>
2018-02-20 11:33:48 -0500

    Add static controller - sets a joint to a fixed value when started

---
+++ zebROS_ws/src/static_controller/src/static_controller.cpp
@@ -0,0 +1,44 @@
+
+#include <ros/node_handle.h>
+#include <static_controller/static_controller.h>
+#include <pluginlib/class_list_macros.h>
+
+
+namespace static_controller
+{
+bool StaticController::init(hardware_interface::JointCommandInterface *hw, ros::NodeHandle &n)
+{
+	std::string joint_name;
+	if (!n.getParam("joint", joint_name))
+	{
+		ROS_ERROR("No joint given (namespace: %s)", n.getNamespace().c_str());
+		return false;
+	}
+	if (!n.getParam("value", value_))
+	{
+		ROS_ERROR("No value given (namespace: %s)", n.getNamespace().c_str());
+		return false;
+	}
+	handle_ = hw->getHandle(joint_name);
+	return true;
+
+}
+
+void StaticController::starting(const ros::Time& /*time*/)
+{
+	handle_.setCommand(value_);
+}
+
+void StaticController::update(const ros::Time & /*time*/, const ros::Duration & /*period*/)
+{
+}
+
+}
+
+#if 0
+void static_controller::update()
+{
+}
+#endif
+
+PLUGINLIB_EXPORT_CLASS(static_controller::StaticController,controller_interface::ControllerBase)
