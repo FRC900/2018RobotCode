3a29ea411d48996874dce51ff3a04379a3aa7916
Kevin Jaget <kjaget@gmail.com>
2018-03-01 09:19:29 -0500

    Fix startup rio_bashrc path stuff

--- rio_bashrc.sh
+++ rio_bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-source ~/2018RobotCode/zebROS_ws/install_isolated/setup.bash 
+source /home/admin/2018RobotCode/zebROS_ws/install_isolated/setup.bash 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/admin/wpilib:/usr/local/lib
 export ROS_MASTER_URI=http://10.9.0.8:5802
 export ROS_IP=10.9.0.2
