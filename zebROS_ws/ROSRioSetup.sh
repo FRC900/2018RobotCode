8a7d32d4c4222b0a2da8be5bb9b1f6fb2a2116f2
Kevin Jaget <kjaget@gmail.com>
2018-03-02 10:37:27 -0500

    Use /bin/hostname rather than hostname in ROS.sh files

--- zebROS_ws/ROSRioSetup.sh
+++ zebROS_ws/ROSRioSetup.sh
@@ -4,7 +4,7 @@
 source /opt/ros/kinetic/setup.bash
 source ~/2018RobotCode/zebROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://10.9.0.2:5802
-export ROS_IP=`hostname -I | tr -d ' ' | tr -d '\n'`
+export ROS_IP=`/bin/hostname -I | tr -d ' ' | tr -d '\n'`
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 exec "$@"
