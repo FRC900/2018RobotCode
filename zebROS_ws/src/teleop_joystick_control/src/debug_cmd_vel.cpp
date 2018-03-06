bc9c1a8b9400f0dcfae6bb9527bdd11bc79691d2
o-fugi <o.fugikawa@gmail.com>
2018-03-05 22:41:00 -0500

    Added messages from frcrobot_hw_interface

--- zebROS_ws/src/teleop_joystick_control/src/debug_cmd_vel.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/debug_cmd_vel.cpp
@@ -5,27 +5,39 @@
 #include <teleop_joystick_control/teleopJoystickCommands.h>
 #include <ros_control_boilerplate/JoystickState.h>
 
-void headerCallback(const std_msgs::Header &msg)
-{
-	static double average_delay = 0;
-	static int count = 0;
+static int teleop_delay;
+static double teleop_averages;
+static int hw_delay;
+static double hw_averages;
 
+//publish delays = [.5, .4, .8, .2]
+void teleopHeaderCallback(const std_msgs::Header &msg)
+{
+	teleop_delay = msg.stamp.sec;
+	/*
+	static average_delay = 0;
+	static int count = 0;
 	count++;
-	if (count == 100)
-	{
-		ROS_INFO_STREAM("delay = " << average_delay << ". seq = " << msg.seq);
-		count = 0;
-		average_delay = 0;
-	}
-	else 
-	{ 
-		average_delay += (ros::Time::now().toSec() - msg.stamp.sec) / 100.0;
-	}
+
+		if (count == 100)
+		{
+			ROS_INFO_STREAM("delay = " << average_delay << ". location = " << msg.sec);
+			count = 0;
+			average_delay = 0;
+		}
+		else 
+		{ 
+			average_delay += (ros::Time::now().toSec() - msg.stamp.sec) / 100.0;
+		}
+
+	*/
 }
 
 void HWHeaderCallback(const ros_control_boilerplate::JoystickState &msg)
 {
-	static double other_average_delay = 0;
+	hw_delay = msg.header.stamp.sec;
+	/*
+	static double hw_average_delay = 0;
 	static int other_count = 0;
 
 	other_count++;
@@ -39,16 +51,35 @@ void HWHeaderCallback(const ros_control_boilerplate::JoystickState &msg)
 	{ 
 		other_average_delay += (ros::Time::now().toSec() - msg.header.stamp.sec) / 100.0;
 	}
+	*/
 }
-	
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "debug_cmd_vel");
 	ros::NodeHandle n;
 
-	ros::Subscriber cmd_vel_subscriber1 = n.subscribe("test_header", 3, &headerCallback); //subscribing to teleopJoystickCommands loop
-	ros::Subscriber cmd_vel_subscriber2 = n.subscribe("joystick_states", 1, &HWHeaderCallback); //subscribing to frcrobot_hw_interface loop
+	ros::Subscriber header_subscriber = n.subscribe("test_header", 3, &teleopHeaderCallback);
+	ros::Subscriber header_subscriber2 = n.subscribe("joystick_states", 3, &HWHeaderCallback);
+	
+	
+
+	static int count = 0;
+	count++;
+
+	if (count == 100)
+	{
+		ROS_INFO_STREAM("delay = " << teleop_delay << ". location = joystick");
+		ROS_INFO_STREAM("delay = " << hw_delay << ". location = hw");
+		count = 0;
+		teleop_delay = 0;
+		hw_delay = 0;
+	}
+	else 
+	{ 
+		teleop_averages += (ros::Time::now().toSec() - teleop_delay) / 100.0;
+		hw_averages += (ros::Time::now().toSec() - hw_delay) / 100.0;
+	}
 
 	ros::spin();
 	return 0;
