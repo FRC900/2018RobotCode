bf6baf174c4e453823d25ef3f379605e29b4e52c
MisterPNP <pnp@jgp.net>
2018-03-21 18:52:36 -0400

    update ROSBag.sh to get data from the rosbag and output it in rqt_plot

--- zebROS_ws/ROSBag.sh
+++ zebROS_ws/ROSBag.sh
@@ -4,10 +4,18 @@
 source /opt/ros/kinetic/setup.bash
 source ~/2018RobotCode/zebROS_ws/devel/setup.bash
 
-roscore
+export ROS_MASTER_URI=http://localhost:11311
 
-rosrun rqt_plot
+sleep 3
 
-roslaunch controller_node rosbag_rqt_graphing.launch rosbag_data:=pembrooke_2018-03-16-18-38-47.bag.active data data1:= data2:= x-axis:= 
+#open roscore session
+roscore&
 
-exec"$@"
+sleep 5
+#data output
+rostopic echo /frcrobot/pdp_states/temperature&
+rqt_plot /frcrobot/pdp_states/temperature&
+
+#play the rosbag to link topic(s)
+cd
+gnome-terminal -- rosbag play --clock Downloads/pembrooke_2018-03-16-18-38-47.bag.active
