cc7df7a4e8ddcaf9f32aa8d3bd2b2db2d02da297
o-fugi <o.fugikawa@gmail.com>
2018-03-24 16:25:38 -0400

    Rename works

--- search_bag.sh
+++ search_bag.sh
@@ -1,5 +1,39 @@
+source /opt/ros/kinetic/setup.bash
+
+echo check for reindex
+if [[ $1 == *.active ]]
+then 
+	echo reindexing!!
+	rosbag reindex $1 
+fi
+
+match='match'
+bag='.bag'
+
+echo run the node!
 zebROS_ws/devel/lib/rosbag_scripts/rosbag_scripts_node $1
-grep -Rl whatever temp_file.txt
+
+echo is there data?
+frame_id=$(grep 'frame_id' temp_file.txt)
+if [[ -n frame_id ]]
+then
+	echo yeah dude
+	echo find the match number...
+	matchNumber=$(sed -n 10p temp_file.txt)
+	bag_name=$match$matchNumber$bag
+	if [[ -e $bag_name ]]
+	then 
+		echo it already exists!
+		#cat $1 >> $bag_name 
+	else 
+		echo it doesn\'t exist and i\'m making a new file
+		cp $1 $bag_name
+	fi
+else
+	echo this is empty
+fi
+
+
 #rm temp_file.txt
 
 
