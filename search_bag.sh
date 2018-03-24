3120774ed34a1ce3ff95796c7c5e44206e898c2d
o-fugi <o.fugikawa@gmail.com>
2018-03-24 17:08:48 -0400

    Fixed bug in search_bag

--- search_bag.sh
+++ search_bag.sh
@@ -1,39 +1,41 @@
 source /opt/ros/kinetic/setup.bash
 
-echo check for reindex
-if [[ $1 == *.active ]]
-then 
-	echo Reindexing
-	rosbag reindex $1 
-fi
 
-match='match'
-bag='.bag'
-
-echo Writing data to file
-zebROS_ws/devel/lib/rosbag_scripts/rosbag_scripts_node $1
-
-echo Is this a match?
-frame_id=$(grep 'frame_id' temp_file.txt)
-if [[ -n frame_id ]]
-then
-	echo This has match data
-	matchNumber=$(sed -n 10p temp_file.txt)
-	bag_name=$match$matchNumber$bag
-	if [[ -e $bag_name ]]
+for var in "$@"
+do
+	echo check for reindex
+	if [[ $var == *.active ]]
 	then 
-		echo This match already has a bag file... merging
-		#cat $1 >> $bag_name 
-	else 
-		echo Renaming bag file
-		cp $1 $bag_name
+		echo Reindexing
+		rosbag reindex $var 
 	fi
-else
-	echo This does not have match data.
-fi
 
+	match='match'
+	bag='.bag'
 
-#rm temp_file.txt
+	echo Writing data to file
+	zebROS_ws/devel/lib/rosbag_scripts/rosbag_scripts_node $var
+
+	echo Is this a match?
+	any_data=$(rosbag info $var | grep /frcrobot/match_data)
+	if [ ! -z "$any_data" -a "$any_data" != " " ] 
+	then
+		echo This has match data
+		matchNumber=$(sed -n 10p temp_file.txt)
+		bag_name=$match$matchNumber$bag
+		if [[ -e $bag_name ]]
+		then 
+			echo This match already has a bag file... merging
+			#cat $var >> $bag_name 
+		else 
+			echo Renaming bag file
+			cp $var $bag_name
+		fi
+	else
+		echo This does not have match data.
+	fi
+	#rm temp_file.txt
+done
 
 
 #first, input bag
