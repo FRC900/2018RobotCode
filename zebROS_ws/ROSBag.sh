3fe9e5cf43de84c1b123ab26ef1f6f3e754776d1
MisterPNP <pnp@jgp.net>
2018-03-24 16:53:28 -0400

    add echo statements for debugging

--- zebROS_ws/ROSBag.sh
+++ zebROS_ws/ROSBag.sh
@@ -9,10 +9,15 @@ export ROS_MASTER_URI=http://localhost:11311
 #open roscore session
 roscore&
 
-sleep 5
+sleep 4
 #data output
 rqt_plot /frcrobot/$2 /frcrobot/$3 /frcrobot/$4 /frcrobot/$5 /frcrobot/$6 /frcrobot/$7 /frcrobot/$8 /frcrobot/$9 /frcrobot/$10&
 
 #play the rosbag to link topic(s)
 cd
 gnome-terminal -- rosbag play --clock Downloads/$1
+sleep 15
+gnome-terminal -- rostopic echo $2
+gnome-terminal -- rostopic echo $3
+gnome-terminal -- rostopic echo $4
+gnome-terminal -- rostopic echo $5
