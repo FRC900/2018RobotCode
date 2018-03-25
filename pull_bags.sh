41abbf375b7fe10915fd68985fda8995f41e3aca
o-fugi <o.fugikawa@gmail.com>
2018-03-25 09:48:17 -0400

    Fixed error

--- pull_bags.sh
+++ pull_bags.sh
@@ -1,6 +1,6 @@
 JETSON_ADDR=10.9.0.8
 
-ssh $JETSON_ADDR "cd 2018RobotCode && \
+ssh $JETSON_ADDR "cd /mnt/900_2/ && \
 	./search_bag.sh /mnt/900_2/_2018*"
 
 scp ubuntu@10.9.0.8:/mnt/900_2/match* .
