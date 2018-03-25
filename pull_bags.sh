a785bd803d30c985dadf7bbb66b41fd707af752d
o-fugi <o.fugikawa@gmail.com>
2018-03-25 09:46:54 -0400

    Maybe pull_bags will work now

--- pull_bags.sh
+++ pull_bags.sh
@@ -1,11 +1,6 @@
 JETSON_ADDR=10.9.0.8
-ip=`/bin/hostname -I | tr -d ' ' | tr -d '\n'`
-
-mkdir ~/bagfiles_from_jetson/
-name='whoami'
-at='@'
-directory=':/home/'
 
 ssh $JETSON_ADDR "cd 2018RobotCode && \
-	./search_bag.sh /mnt/900_2/_2018* && \
-	scp match* $name$at$ip$directory$name"
+	./search_bag.sh /mnt/900_2/_2018*"
+
+scp ubuntu@10.9.0.8:/mnt/900_2/match* .
