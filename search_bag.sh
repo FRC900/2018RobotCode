97cf1d47a81273beb65ba508f36347b06f3e9a5a
o-fugi <o.fugikawa@gmail.com>
2018-03-26 21:05:25 -0400

    More progress on reading rosbags

--- search_bag.sh
+++ search_bag.sh
@@ -1,7 +1,11 @@
+#also the organization is horrible. TODO please please fix
+
 source /opt/ros/kinetic/setup.bash
 
 IFS='
 '
+
+count=1
 
 if [ $# -eq 0 ]
 then
@@ -10,7 +14,8 @@ fi
 
 for var in "$@"
 do
-	echo check for reindex
+	echo For bag file ${var}:
+
 	if [[ $var == *bag.active ]]
 	then 
 		echo Reindexing
@@ -23,42 +28,45 @@ do
 		continue
 	fi
 
-	match='match'
-	bag='.bag'
-
 	echo Writing data to file
 	zebROS_ws/devel/lib/rosbag_scripts/rosbag_scripts_node $var
 	filename=$(basename $var)
 
-	echo Is this a match?
 	any_data=$(rosbag info $var | grep /frcrobot/match_data)
 	if [ ! -z "$any_data" -a "$any_data" != " " ] 
 	then
 		echo This has match data
-		matchNumber=$(sed -n 10p temp_file.txt)
-		if [ $matchNumber = 0 ]
-		then
-			echo Match number is zero -- skipping
-			continue
-		fi
-		bag_name=$match$matchNumber$bag
-		if [[ -e $bag_name ]]
+		alliance_data=$(grep this temp_file.txt)
+		if [ ! -z "$alliance_data" -a "$alliance_data" != " " ]
 		then 
-			echo This match already has a bag file... merging
-			~/2018RobotCode/zebROS_ws/src/rosbag_scripts/scripts/merge_bagfiles.py $bagname $var $bag_name -v 
-		else 
-			echo Renaming bag file
-			cp $var $bag_name
+			echo This has alliance data.
+			matchNumber=$(grep -m1 matchNumber temp_file.txt | cut -c14-15)
+			if [ $matchNumber = 0 ]
+			then
+				echo Match number is zero -- renaming
+				mv $var $HOME/2018RobotCode/match0$(basename $var)
+				continue
+			fi
+			bag_name=Match${matchNumber}
+			if [ -e ${bag_name}*.bag ]
+			then 
+				echo This match already has a bag file -- merging to ${bag_name}_${count}.bag
+				~/2018RobotCode/zebROS_ws/src/rosbag_scripts/scripts/merge_bagfiles.py ${bag_name}_${count}.bag $var ${bag_name}*.bag -v
+				rm ${bag_name}_$(( $count - 1)).bag
+				rm ${bag_name}.bag
+				count=$(( $count + 1 ))
+			else 
+				echo Renaming bag file to ${bag_name}.bag
+				cp $var $HOME/2018RobotCode/${bag_name}.bag
+			fi
+		else
+			echo This does not have alliance data -- renaming to prematch${var}
+			cp $var $HOME/2018RobotCode/prematch$(basename $var)
 		fi
 	else
-		echo This does not have match data -- renaming
-		mv $var /mnt/900_2/practice$(basename $var)
+		echo This does not have match data -- renaming to practice${var} 
+		cp $var $HOME/2018RobotCode/practice$(basename $var)
 	fi
-	rm temp_file.txt
+	#rm temp_file.txt
+
 done
-
-
-#first, input bag
-#second, run search_bag.cpp. it creates a temp file with all match_data text.
-#then, search that file for actual data. 
-#if returns true, rename the bag to fit the match data. if not, do not rename the bag
