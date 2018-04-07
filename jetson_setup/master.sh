cafe7bab51487881cd27681749eb96c951effb97
Ryan Greenblatt <greenblattryan@gmail.com>
2018-04-07 10:04:22 -0400

    Fixed yaml/launhc

--- jetson_setup/master.sh
+++ jetson_setup/master.sh
@@ -20,9 +20,13 @@ export CUDA_CACHE_MAXSIZE=104857600
 export CUDA_CACHE_PATH=/home/ubuntu/.nv/ComputeCache
 
 if sudo mount /dev/nvme0n1p1 /mnt/900_2; then
+		date >> /home/ubuntu/mounted.txt
+		echo worked! >> /home/ubuntu/mounted.txt
 		sudo chmod a+rw /mnt/900_2/
 		roslaunch controller_node controller_master.launch record:=true
 else
+		date >> /home/ubuntu/mounted.txt
+		echo did not mount >> /home/ubuntu/mounted.txt
 		roslaunch controller_node controller_master.launch
 fi
 
