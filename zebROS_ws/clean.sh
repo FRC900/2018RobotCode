b1faff8164b8dad716404b1f1135fad0b22cd444
Kevin Jaget <kjaget@gmail.com>
2018-04-09 15:13:03 -0400

    More updates for native build script

--- zebROS_ws/clean.sh
+++ zebROS_ws/clean.sh
@@ -1,5 +1,1 @@
-sudo rm -rf ./devel/
-sudo rm -rf ./devel_isolated/
-sudo rm -rf ./build/
-sudo rm -rf ./build_isolated/
-sudo rm -rf ./install_isolated/
+sudo rm -rf devel devel_isolated build build_isolated install_isolated native_build native_devel native_install
--- zebROS_ws/native_build.sh
+++ zebROS_ws/native_build.sh
@@ -1,1 +1,1 @@
-catkin_make_isolated --install --use-ninja --build-space native_isolated --devel-space native_devel --install-space native_install -DCMAKE_BUILD_TYPE=Release -DCATKIN_ENABLE_TESTING=OFF "$@"
+catkin_make_isolated --install --use-ninja --build-space native_build --devel-space native_devel --install-space native_install -DCMAKE_BUILD_TYPE=Release -DCATKIN_ENABLE_TESTING=OFF "$@"
