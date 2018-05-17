9b19894efb4e9436079291af4cecd921caf98f82
Kevin Jaget <kjaget@gmail.com>
2018-05-17 18:43:19 -0400

    roborio install is now an actual script

--- roboio_install_ros.sh
+++ roboio_install_ros.sh
@@ -1,207 +1,100 @@
 #!/bin/bash
 
-#sed -i 's#nickdanger\.amer\.corp\.natinst\.com/feeds/\([^/]\+\)/\([^/]\+\)/#download.ni.com/ni-linux-rt/feeds/\1/\2/ipk/#' /etc/opkg/base-feeds.conf
-opkg update
+# Run me with IP address of Rio as argument
+
+# Set time and date on Rio to match the system
+# we're installing from.  This should be close enough
+# to reality to get past ssl errors from pip if the 
+# date on the Rio is totally wacky
+ssh admin@$1 date --set="\"`date -R`\"" 
+
+ssh admin@$1 'opkg update'
 
 # Split these up so the disk doesn't fill up with temp files
 # Also need to install pyyaml first for some reason to avoid
 # weird dependency hell issues with opkg
-opkg install python-pyyaml
-opkg clean
-opkg install libeigen python-dev libpython2 python-core 
-opkg clean
-opkg install libcurl4 lz4 libboost-filesystem1.60.0 libboost-program-options1.60.0 libboost-signals1.60.0 libboost-regex1.60.0 libboost-thread1.60.0 libboost-chrono1.60.0 libboost-date-time1.60.0 libboost-atomic1.60.0
-opkg clean
-opkg install libbz2 cmake libxml2 libgnutls-bin libgnutls-openssl27  
-opkg clean
-opkg install libgnutls30 libgnutlsxx28 nettle libgmp10 libgmpxx4 libz1 git make 
-opkg clean
-opkg install gcc g++ gcc-symlinks g++-symlinks binutils python-setuptools python-docutils 
-opkg clean
-opkg install python-pkgutil python-dateutil python-argparse python-nose 
-opkg clean
-opkg install python-netifaces libglog0 python-pip coreutils gdb ntp i2c-tools 
-opkg clean
-opkg install ntp ntp-tickadj ntp-utils ntpd ntpdate rsync htop curl
-opkg clean
+ssh admin@$1 'opkg install python-pyyaml'
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install libeigen python-dev libpython2 python-core '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install libcurl4 lz4 libboost-filesystem1.60.0 libboost-program-options1.60.0 libboost-signals1.60.0 libboost-regex1.60.0 libboost-thread1.60.0 libboost-chrono1.60.0 libboost-date-time1.60.0 libboost-atomic1.60.0'
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install libbz2 cmake libxml2 libgnutls-bin libgnutls-openssl27  '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install libgnutls30 libgnutlsxx28 nettle libgmp10 libgmpxx4 libz1 git make '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install gcc g++ gcc-symlinks g++-symlinks binutils python-setuptools python-docutils '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install python-pkgutil python-dateutil python-argparse python-nose '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install python-netifaces libglog0 python-pip coreutils gdb ntp i2c-tools '
+ssh admin@$1 'opkg clean'
+ssh admin@$1 'opkg install ntp ntp-tickadj ntp-utils ntpdate rsync htop curl'
+ssh admin@$1 'opkg clean'
 
-# set RIO date before running this to prevent
-# SSL certificate errors
-pip install catkin_pkg rospkg rosdistro vcstools rosdep wstool rosinstall rosinstall_generator defusedxml empy
+ssh admin@$1 'pip install catkin_pkg rospkg rosdistro vcstools rosdep wstool rosinstall rosinstall_generator defusedxml empy'
 
 # Copy over ROS tar.bz2 file, extract to /
-source /opt/ros/kinetic/setup.bash
+# Maybe get smart and wget it into /tmp from the google drive
+# link and then install from there
+scp ~/roscore_roborio_2018.tar.bz2 admin@$1:.
+ssh admin@$1 'cd / && tar -xjf ~/roscore_roborio_2018.tar.bz2'
+scp ~/2018RobotCode/os_detect.py admin@$1:/usr/lib/python2.7/site-packages/rospkg/
+ssh admin@$1 'rm ~/roscore_roborio_2018.tar.bz2'
 
 # Try to simulate what the cross-build environment
 # looks like 
-ln -s / /usr/arm-frc-linux-gnueabi
-ln -s /usr/include /include
+ssh admin@$1 'ln -s / /usr/arm-frc-linux-gnueabi'
+ssh admin@$1 'ln -s /usr/include /include'
 
-cd
-mkdir -p 2018RobotCode/zebROS_ws/src
-cd 2018RobotCode/zebROS_ws
-catkin_make_isolated --install
-cd 
+ssh admin@$1 'mkdir -p 2018RobotCode/zebROS_ws/src'
+ssh admin@$1 'source /opt/ros/kinetic/setup.bash && cd 2018RobotCode/zebROS_ws && catkin_make_isolated --install'
+
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 ##################-----------------------------#################
 #Edit /etc/ntp.conf to be a copy of ntp-server in 2018RobotCode#
-##################-----------------------------#################
-#
-#
-#
+scp ~/2018RobotCode/ntp-server.conf admin@$1:/etc/ntp.conf
+
 #---------------------------------------------------------------#
 #               to setup RTC on the rio
 #   Plug the rtc into the i2c port on the rio while unpowered
-#   copy rtc-bq32.ko
 #
-#
-#cp rtc-bq32k.ko /lib/modules/`uname -r`/kernel
-#depmod
-#i2cdetect -y 2
-#echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device
+scp ~/2018RobotCode/rtc-bq32k.ko admin@$1:.
+ssh admin@$1 'mv rtc-bq32k.ko /lib/modules/`uname -r`/kernel'
+ssh admin@$1 'depmod'
+ssh admin@$1 'i2cdetect -y 2'
+ssh admin@$1 'echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device'
 
-cd
-git clone https://github.com/gflags/gflags.git
-cd gflags
-cmake -DCMAKE_BUILD_TYPE=Release .
-make install
-cd
-rm -rf gflags*
-
-#scp ~/2018RobotCode/os_detect.py admin@<target>:/usr/lib/python2.7/site-packages/rospkg/
+ssh admin@$1 'git clone https://github.com/gflags/gflags.git'
+ssh admin@$1 'cd gflags && cmake -DCMAKE_BUILD_TYPE=Release . &&  make install'
+ssh admin@$1 'rm -rf gflags*'
 
 # Copy wpilib to roborio
-# cd ~/wpilib/cpp/current/reflib/linux/athena/shared
-#scp *.so.* <target>:wpilib
-#cd ~/wpilib/common/current/lib/linux/athena/shared
-#scp *.so *.so.3.2 <target>:wpilib
+cd ~/wpilib/cpp/current/reflib/linux/athena/shared
+scp *.so.* admin@$1:wpilib
+cd ~/wpilib/common/current/lib/linux/athena/shared
+scp *.so *.so.3.2 admin@$1:wpilib
 
-echo << EOF > setupClock
-#!/bin/bash
-NAME="Setup Clock"
-USER=admin
-depmod
-echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device
-retries=0
-hwclock.util-linux --utc --hctosys
-while [[ $? -ne 0 && $retries < 5 ]]; do
-    sleep 5
-    retries+=1
-    hwclock.util-linux --utc --hctosys
-done
-EOF
+scp ~/2018RobotCode/setupClock admin@$1:/etc/init.d
+ssh admin@$1 'chmod +x /etc/init.d/setupClock'
+ssh admin@$1 'ln -sf /etc/init.d/setupClock /etc/init.d/hwclock.sh'
+ssh admin@$1 '/usr/sbin/update-rc.d -f setupClock defaults'
+ssh admin@$1 '/usr/sbin/update-rc.d -f hwclock.sh defaults'
 
-chmod +x setupClock
-cp setupClock /etc/init.d
-ln -s /etc/init.d/setupClock /etc/init.d/hwclock.sh
-/usr/sbin/update-rc.d -f setupClock defaults
-/usr/sbin/update-rc.d -f hwclock.sh defaults
-
-
-cd
-mkdir .ssh
-#scp jetson_setup/roborio_dot_ssh.tar.bz2 admin@<target>
-cd .ssh
-tar -xjf ../roborio_dot_ssh.tar.bz2
+scp ~/2018RobotCode/jetson_setup/roborio_dot_ssh.tar.bz2 admin@$1:.
+ssh admin@$1 'mkdir .ssh'
+ssh admin@$1 'cd .ssh && tar -xjf ../roborio_dot_ssh.tar.bz2'
+ssh admin@$1 'rm roborio_dot_ssh.tar.bz2'
 
 # Edit /etc/ssh/sshd_config, uncomment Port 22, add Port 5801, 
 # uncomment ChallengeResponseAuthentication and set it to no
 
+# Copy rio_bashrc.sh, ROSJetsonMaster.sh to /home/admin
+scp ~/2018RobotCode/rio_bashrc.sh admin@$1:.
+scp ~/2018RobotCode/zebROS_ws/ROSJetsonMaster.sh admin@$1:.
+
 # Set up prereqs for deploy script
-mv ~/2018RobotCode ~/2018RobotCode.orig
-ln -s ~/2018RobotCode.orig ~/2018RobotCode
-mkdir -p ~/2018RobotCode.prod/zebROS_ws
-mkdir -p ~/2018RobotCode.dev/zebROS_ws
-
-#*******************************************************
-# STOP HERE
-#*******************************************************
-
-# Changed this to static lib on host, shouldn't need
-# to be installed on target as well
-#cd
-#git clone https://github.com/ros/console_bridge
-#cd console_bridge
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=Release ..
-#make install
-#cd
-#rm -rf console_bridge
-
-# Changed to static libs on host, shouldn't be needed on target
-
-# This is pre-built on a flash drive - make install should just 
-# check everything is up to date and then copy the libraries and headers
-# over ... at least until the Rio image changes?
-#cd
-#wget https://pocoproject.org/releases/poco-1.7.9/poco-1.7.9p1.tar.gz
-#tar -xzf poco-1.7.9p1.tar.gz 
-#cd poco-1.7.9p1/
-#./configure --no-tests --no-samples --omit=Data/ODBC,Data/MySQL --minimal
-#cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_MONGODB=OFF -DENABLE_CRYPTO=OFF -DENABLE_NET=OFF -DENABLE_NETSSL=OFF -DENABLE_DATA=OFF -DENABLE_ZIP=OFF -DENABLE_PAGECOMPILER=OFF -DENABLE_PAGECOMPILER_FILE2PAGE=OFF .
-#make -j2 install
-#cd
-#rm -rf poco-1.7.9p1 poco-1.7.9p1.tar.gz 
-
-# KCJ - I'm skeptical any of the below libs are really
-# needed.  Many of the produce static libs so installed
-# ROS components are already linked against them during
-# the cross-build process.  Others are used by esoteric
-# packages we'll probably never bother with.  If we end
-# up getting a missing library error, though, the info
-# on how to build them is here
-# cd
-# wget https://downloads.sourceforge.net/project/pyqt/sip/sip-4.17/sip-4.17.tar.gz
-# tar -xzvf sip-4.17.tar.gz
-# cd sip-4.17
-# python configure.py
-# make -j2 install
-# cd
-# rm -rf sip-4.17*
-
-# Probably not needed since it produces a static library
-# cd
-# wget https://downloads.sourceforge.net/project/tinyxml/tinyxml/2.6.2/tinyxml_2_6_2.zip
-# unzip tinyxml_2_6_2.zip 
-# cd tinyxml
-# wget https://gist.githubusercontent.com/TNick/7960323/raw/3046ecda1d4d54d777c407f43ac357846a192e05/TinyXML-CmakeLists.txt
-# mv TinyXML-CmakeLists.txt CMakeLists.txt
-# add a line to CMakeLists.txt :  
-  # set_target_properties(tinyxml PROPERTIES PUBLIC_HEADER "tinyxml.h;tinystr.h")
-# add a line to tinyxml.h before line 46 :
-  # #define TIXML_USE_STL
-# cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON .
-# make -j2 install 
-# cd
-# rm -rf tinyxml*
-
-# cd
-# wget http://www.qhull.org/download/qhull-2015-src-7.2.0.tgz
-# tar -xzvf qhull-2015-src-7.2.0.tgz
-# cd qhull-2015.2/
-# cmake -DCMAKE_BUILD_TYPE=Release .
-# make -j2 install
-# cd
-# rm -rf qhull-2015*
-
-# cd
-# git clone https://github.com/assimp/assimp.git 
-# cd assimp
-# mkdir build
-# cd build
-# cmake -DCMAKE_BUILD_TYPE=Release .
-# make -j2 install
-# cd
-# rm -rf assimp
-
-
-# cd
-# wget https://downloads.sourceforge.net/project/libuuid/libuuid-1.0.3.tar.gz
-# tar -xzvf libuuid-1.0.3.tar.gz
-# cd libuuid-1.0.3/
-# ./configure 
-# make -j2 install
-# cd
-# rm -rf libuuid-1.0.3*
-
+ssh admin@$1 'mv ~/2018RobotCode ~/2018RobotCode.orig'
+ssh admin@$1 'ln -s ~/2018RobotCode.orig ~/2018RobotCode'
+ssh admin@$1 'mkdir -p ~/2018RobotCode.prod/zebROS_ws'
+ssh admin@$1 'mkdir -p ~/2018RobotCode.dev/zebROS_ws'
