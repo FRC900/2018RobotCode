c1ffdc70b5eda919edbadf2366747c42f40efdaa
FalconWarriorr <niall.p.mullane@gmail.com>
2018-06-06 17:03:52 -0400

    added standard rqt plugins to launch_ros.sh

--- zebROS_ws/launch_ros.sh
+++ zebROS_ws/launch_ros.sh
@@ -20,6 +20,22 @@ launch_driver_station()
 {
     rqt --standalone rqt_driver_station_sim &
 }
+launch_rqt_plot()
+{
+    rosrun rqt_plot rqt_plot &
+}
+launch_rqt_graph()
+{
+    rosrun rqt_graph rqt_graph &
+}
+launch_rqt_gui()
+{
+    rosrun rqt_gui rqt_gui &
+}
+launch_rqt_console()
+{
+    rosrun rqt_console rqt_console &
+}
 
 run_sim=0
 run_code=0
@@ -34,10 +50,14 @@ for i in $@; do
         echo -e "\t ./launch_ros.sh rviz \t\t # Launches default rviz"
         echo -e "\t ./launch_ros.sh arm_viz.rviz \t # Launches arm_viz.rviz rviz file \n"
         echo -e "Possible options: \n"
-        echo -e "\t sim \t-> launches ROS sim code"
-        echo -e "\t robot \t-> launches robot code"
-        echo -e "\t rviz \t-> launches default rviz"
-        echo -e "\t ds \t-> launches RQT driver station sim"
+        echo -e "\t sim \t\t-> launches ROS sim code"
+        echo -e "\t robot \t\t-> launches robot code"
+        echo -e "\t rviz \t\t-> launches default rviz"
+        echo -e "\t rqt_plot \t-> launches rqt_plot"
+        echo -e "\t rqt_graph \t-> launches rqt_graph"
+        echo -e "\t rqt_console \t-> launches rqt_console"
+        echo -e "\t rqt_gui \t-> launches rqt_gui"
+        echo -e "\t ds \t\t-> launches RQT driver station sim"
         echo -e "\t any other argument is interpreted as a .rviz config file"
     elif [ $i = "rviz" ]; then
         launch_rviz
@@ -47,6 +67,14 @@ for i in $@; do
         run_code=1
     elif [ $i = "ds" ]; then
         launch_driver_station
+    elif [ $i = "rqt_plot" ]; then
+        launch_rqt_plot
+    elif [ $i = "rqt_graph" ]; then
+        launch rqt_graph
+    elif [ $i = "rqt_console" ]; then
+        launch rqt_console
+    elif [ $i = "rqt_gui" ]; then
+        launch rqt_gui
     else
         launch_specific_rviz $i
     fi
